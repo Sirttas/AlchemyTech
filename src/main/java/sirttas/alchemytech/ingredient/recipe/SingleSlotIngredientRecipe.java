a34c4a1987954d80bbc38d6ab28b1a98865a3c27
Gaël Calloch <calloch.gael@gmail.com>
2016-07-29 19:47:55 +0200

    Generic ingredient recipes

---
+++ src/main/java/sirttas/alchemytech/ingredient/recipe/SingleSlotIngredientRecipe.java
@@ -0,0 +1,55 @@
+package sirttas.alchemytech.ingredient.recipe;
+
+import net.minecraft.item.ItemStack;
+import sirttas.alchemytech.block.tile.instrument.TileInstrument;
+import sirttas.alchemytech.ingredient.Ingredient;
+import sirttas.alchemytech.item.ItemPreparation;
+
+public abstract class SingleSlotIngredientRecipe<T extends TileInstrument> implements IIngredientRecipe<T> {
+
+	protected Ingredient[] output;
+	protected Ingredient[] input;
+
+	public SingleSlotIngredientRecipe(Ingredient output, Ingredient input) {
+		this(output, new Ingredient[] { input });
+	}
+
+	public SingleSlotIngredientRecipe(Ingredient output, Ingredient[] input) {
+		this(new Ingredient[] { output }, input);
+	}
+
+	public SingleSlotIngredientRecipe(Ingredient[] output, Ingredient input) {
+		this(output, new Ingredient[] { input });
+	}
+
+	public SingleSlotIngredientRecipe(Ingredient[] output, Ingredient[] input) {
+		this.output = output;
+		this.input = input;
+	}
+
+
+	@Override
+	public void process(T instrument) {
+		ItemStack stack = instrument.getStackInSlot(0);
+		ItemPreparation preparation = (ItemPreparation) stack.getItem();
+
+		if (isAvalable(instrument)) {
+			for (Ingredient ingredient : input) {
+				preparation.removeIngredient(stack, ingredient);
+			}
+			for (Ingredient ingredient : output) {
+				preparation.addIngredient(stack, ingredient);
+			}
+
+		}
+
+	}
+
+	@Override
+	public boolean isAvalable(T instrument) {
+		ItemStack stack = instrument.getStackInSlot(0);
+
+		return stack != null && stack.getItem() instanceof ItemPreparation
+				&& ((ItemPreparation) stack.getItem()).hasAllIngredient(stack, input);
+	}
+}
--- src/main/java/sirttas/alchemytech/ingredient/recipe/instrument/ShakerRecipe.java
+++ src/main/java/sirttas/alchemytech/ingredient/recipe/instrument/ShakerRecipe.java
@@ -1,53 +1,25 @@
 package sirttas.alchemytech.ingredient.recipe.instrument;
 
-import net.minecraft.item.ItemStack;
 import sirttas.alchemytech.block.instrument.shaker.TileShaker;
 import sirttas.alchemytech.ingredient.Ingredient;
-import sirttas.alchemytech.ingredient.recipe.IIngredientRecipe;
-import sirttas.alchemytech.item.ItemPreparation;
+import sirttas.alchemytech.ingredient.recipe.SingleSlotIngredientRecipe;
 
-public class ShakerRecipe implements IIngredientRecipe<TileShaker> {
-
-	Ingredient[] output;
-	Ingredient[] input;
+public class ShakerRecipe extends SingleSlotIngredientRecipe<TileShaker> {
 
 	public ShakerRecipe(Ingredient output, Ingredient input) {
-		this(output, new Ingredient[] { input });
+		super(output, input);
 	}
 
 	public ShakerRecipe(Ingredient output, Ingredient[] input) {
-		this(new Ingredient[] { output }, input);
+		super(output, input);
+	}
+
+	public ShakerRecipe(Ingredient[] output, Ingredient input) {
+		super(output, input);
 	}
 
 	public ShakerRecipe(Ingredient[] output, Ingredient[] input) {
-		this.output = output;
-		this.input = input;
-	}
-
-
-	@Override
-	public void process(TileShaker shaker) {
-		ItemStack stack = shaker.getStackInSlot(0);
-		ItemPreparation preparation = (ItemPreparation) stack.getItem();
-
-		if (isAvalable(shaker)) {
-			for (Ingredient ingredient : input) {
-				preparation.removeIngredient(stack, ingredient);
-			}
-			for (Ingredient ingredient : output) {
-				preparation.addIngredient(stack, ingredient);
-			}
-
-		}
-
-	}
-
-	@Override
-	public boolean isAvalable(TileShaker shaker) {
-		ItemStack stack = shaker.getStackInSlot(0);
-
-		return stack != null && stack.getItem() instanceof ItemPreparation
-				&& ((ItemPreparation) stack.getItem()).hasAllIngredient(stack, input);
+		super(output, input);
 	}
 
 }
