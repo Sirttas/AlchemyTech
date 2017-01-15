996b46e84bef79784ce82818390013a67ca1b7ed
Sirttas <calloch.gael@gmail.com>
2017-01-15 07:57:11 +0100

    bottle in filter

--- src/main/java/sirttas/alchemytech/block/instrument/filter/FilterRenderer.java
+++ src/main/java/sirttas/alchemytech/block/instrument/filter/FilterRenderer.java
@@ -1,11 +1,16 @@
 package sirttas.alchemytech.block.instrument.filter;
 
+import static sirttas.alchemytech.block.BlockAT.BIT_SIZE;
+
 import sirttas.alchemytech.block.tile.renderer.InstrumentRenderer;
 
 public class FilterRenderer extends InstrumentRenderer<TileFilter> {
 
 	@Override
 	public void renderTileEntityAt(TileFilter te, double x, double y, double z, float partialTicks, int destroyStage) {
+		if (te.getStackInSlot(0) != null) {
+			renderItem(te.getStackInSlot(0), x + 0.475, y + 6 * BIT_SIZE, z + 0.475, 0.5);
+		}
 	}
 
 }
