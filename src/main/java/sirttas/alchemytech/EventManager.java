6eafc38f75ebc46b11ee6e07780cdc78487a9308
Sirttas <calloch.gael@gmail.com>
2017-01-17 22:11:05 +0100

    spawn light block

--- src/main/java/sirttas/alchemytech/EventManager.java
+++ src/main/java/sirttas/alchemytech/EventManager.java
@@ -2,10 +2,12 @@ package sirttas.alchemytech;
 
 import java.util.Random;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import sirttas.alchemytech.block.ATBlocks;
 
 public class EventManager {
 	Random random = new Random();
@@ -15,9 +17,16 @@ public class EventManager {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onLivingTick(LivingUpdateEvent event) {
 		NBTTagCompound data = event.getEntityLiving().getEntityData();
+		EntityLivingBase entity = event.getEntityLiving();
 
 		if (data.hasKey(GLOWING)) {
 			data.setInteger(GLOWING, data.getInteger(GLOWING) - 1);
+			if (entity.getEntityWorld().isAirBlock(entity.getPosition())) {
+				entity.getEntityWorld().setBlockState(entity.getPosition(), ATBlocks.glowingEssence.getDefaultState());
+			}
+			if (data.getInteger(GLOWING) == 0) {
+				data.removeTag(GLOWING);
+			}
 		}
 	}
 
