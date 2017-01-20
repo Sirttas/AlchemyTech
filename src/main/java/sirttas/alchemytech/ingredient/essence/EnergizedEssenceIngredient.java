91746621e3ca2ce76c5e170837fbb4f83e2515df
Sirttas <calloch.gael@gmail.com>
2017-01-20 08:39:39 +0100

    energized essence effect

--- src/main/java/sirttas/alchemytech/ingredient/essence/EnergizedEssenceIngredient.java
+++ src/main/java/sirttas/alchemytech/ingredient/essence/EnergizedEssenceIngredient.java
@@ -1,10 +1,15 @@
 package sirttas.alchemytech.ingredient.essence;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
 public class EnergizedEssenceIngredient extends EssenceIngredient {
 
 	private static final String NAME = "energized";
+	private PotionEffect speed;
+	private PotionEffect strenth;
 
 	public EnergizedEssenceIngredient() {
 		super(NAME);
@@ -12,6 +17,16 @@ public class EnergizedEssenceIngredient extends EssenceIngredient {
 
 	@Override
 	public void applyOnEntity(Entity entity, int strenth) {
-		// entity.getEntityData().setInteger("GLOWING", 200);
+		if (entity instanceof EntityLivingBase) {
+			EntityLivingBase livingEntity = (EntityLivingBase) entity;
+
+			if (speed == null || this.strenth == null) {
+				speed = new PotionEffect(Potion.getPotionFromResourceLocation("minecraft:speed"), 10, 1);
+				this.strenth = new PotionEffect(Potion.getPotionFromResourceLocation("minecraft:strenth"), 10, 1);
+			}
+
+			livingEntity.addPotionEffect(speed);
+			livingEntity.addPotionEffect(this.strenth);
+		}
 	}
 }
