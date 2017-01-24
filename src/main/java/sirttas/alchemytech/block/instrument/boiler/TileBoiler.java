5a0ba386962ec447f73e1f9f0df752267f010672
Sirttas <calloch.gael@gmail.com>
2017-01-24 17:44:48 +0100

    new ingot images

--- src/main/java/sirttas/alchemytech/block/instrument/boiler/TileBoiler.java
+++ src/main/java/sirttas/alchemytech/block/instrument/boiler/TileBoiler.java
@@ -1,7 +1,5 @@
 package sirttas.alchemytech.block.instrument.boiler;
 
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import sirttas.alchemytech.block.tile.instrument.TileSingleSlotInstrument;
 
 public class TileBoiler extends TileSingleSlotInstrument {
@@ -9,13 +7,5 @@ public class TileBoiler extends TileSingleSlotInstrument {
 	@Override
 	public String getName() {
 		return ConfigBoiler.UNLOCALIZED_NAME;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	protected void clientUpdate() {
-		if (progress > 0) {
-			progress++;
-		}
 	}
 }
