452653420e9a4aa3e33f2f3554aa7dfcc71d83f0
Sirttas <calloch.gael@gmail.com>
2017-01-24 12:56:12 +0100

    boiler render

--- src/main/java/sirttas/alchemytech/block/instrument/boiler/BoilerRenderer.java
+++ src/main/java/sirttas/alchemytech/block/instrument/boiler/BoilerRenderer.java
@@ -1,13 +1,16 @@
 package sirttas.alchemytech.block.instrument.boiler;
 
+import static sirttas.alchemytech.block.BlockAT.BIT_SIZE;
+
 import sirttas.alchemytech.block.tile.renderer.InstrumentRenderer;
 
 public class BoilerRenderer extends InstrumentRenderer<TileBoiler> {
 
 	@Override
-	public void renderTileEntityAt(TileBoiler te, double x, double y, double z, float partialTicks,
-			int destroyStage) {
-
+	public void renderTileEntityAt(TileBoiler te, double x, double y, double z, float partialTicks, int destroyStage) {
+		if (te.getStackInSlot(0) != null) {
+			renderItem(te.getStackInSlot(0), x + 0.5, y + 8.5 * BIT_SIZE, z + 0.5, 0.5);
+		}
 	}
 
 }
--- src/main/java/sirttas/alchemytech/block/instrument/boiler/TileBoiler.java
+++ src/main/java/sirttas/alchemytech/block/instrument/boiler/TileBoiler.java
@@ -1,5 +1,7 @@
 package sirttas.alchemytech.block.instrument.boiler;
 
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import sirttas.alchemytech.block.tile.instrument.TileSingleSlotInstrument;
 
 public class TileBoiler extends TileSingleSlotInstrument {
@@ -9,4 +11,11 @@ public class TileBoiler extends TileSingleSlotInstrument {
 		return ConfigBoiler.UNLOCALIZED_NAME;
 	}
 
+	@Override
+	@SideOnly(Side.CLIENT)
+	protected void clientUpdate() {
+		if (progress > 0) {
+			progress++;
+		}
+	}
 }
