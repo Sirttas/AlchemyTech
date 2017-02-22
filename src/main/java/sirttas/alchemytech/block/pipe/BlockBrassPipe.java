46f7ec0da2b9e1763de690a8a1c9556239f8928b
Sirttas <calloch.gael@gmail.com>
2017-02-22 14:21:23 +0100

    bugfix

--- src/main/java/sirttas/alchemytech/block/pipe/BlockBrassPipe.java
+++ src/main/java/sirttas/alchemytech/block/pipe/BlockBrassPipe.java
@@ -46,13 +46,13 @@ public class BlockBrassPipe extends BlockAT implements ITileEntityProvider {
 
 	public static final AxisAlignedBB BASE_AABB = new AxisAlignedBB(6.5 * BIT_SIZE, 6.5 * BIT_SIZE, 6.5 * BIT_SIZE,
 			9.5 * BIT_SIZE, 9.5 * BIT_SIZE, 9.5 * BIT_SIZE);
-	public static final AxisAlignedBB EAST_AABB = new AxisAlignedBB(0, 7 * BIT_SIZE, 7 * BIT_SIZE, 6.5 * BIT_SIZE,
+	public static final AxisAlignedBB WEST_AABB = new AxisAlignedBB(0, 7 * BIT_SIZE, 7 * BIT_SIZE, 6.5 * BIT_SIZE,
 			9 * BIT_SIZE, 9 * BIT_SIZE);
 	public static final AxisAlignedBB DOWN_AABB = new AxisAlignedBB(7 * BIT_SIZE, 0, 7 * BIT_SIZE, 9 * BIT_SIZE,
 			6.5 * BIT_SIZE, 9 * BIT_SIZE);
 	public static final AxisAlignedBB NORTH_AABB = new AxisAlignedBB(7 * BIT_SIZE, 7 * BIT_SIZE, 0, 9 * BIT_SIZE,
 			9 * BIT_SIZE, 6.5 * BIT_SIZE);
-	public static final AxisAlignedBB WEST_AABB = new AxisAlignedBB(9.5 * BIT_SIZE, 7 * BIT_SIZE, 7 * BIT_SIZE,
+	public static final AxisAlignedBB EAST_AABB = new AxisAlignedBB(9.5 * BIT_SIZE, 7 * BIT_SIZE, 7 * BIT_SIZE,
 			16 * BIT_SIZE, 9 * BIT_SIZE, 9 * BIT_SIZE);
 	public static final AxisAlignedBB UP_AABB = new AxisAlignedBB(7 * BIT_SIZE, 9.5 * BIT_SIZE, 7 * BIT_SIZE,
 			9 * BIT_SIZE, 16 * BIT_SIZE, 9 * BIT_SIZE);
